088726c157906b067ace850e62225356472096ee
kripesh123 <kripeshbista3@gmail.com>
2018-05-15 18:34:27 +0545

    Create folder structure for utils file and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputFieldTextComponent } from './shared-utils/form-utils/input-field-text/input-field-text.component';
+import { InputFieldNumberComponent } from './shared-utils/form-utils/input-field-number/input-field-number.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFieldTextComponent,
+    InputFieldNumberComponent
   ],
   imports: [
     BrowserModule
